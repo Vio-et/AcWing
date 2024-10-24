255f0e9c0a23828392ccbc6a70fffd865d1cca47
Orlando <1669785222@qq.com>
2024-10-24 18:58:50 +0800

    merger sort

---
+++ Base/1-BasicAlgorithm/AC787.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AC787 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(), array[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+            array[i] = scanner.nextInt();
+
+        merge_sort(array, 0, n - 1);
+
+        for(int i = 0; i < n; i++)
+            System.out.print(array[i] + " ");
+
+        scanner.close();
+    }
+
+    private static void merge_sort(int[] array, int l, int r){
+        if(l >= r)
+            return;
+        
+        int x = array[l + r >> 1], tmp[] = new int[r - l + 1];
+    }
+}
