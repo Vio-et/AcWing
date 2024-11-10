f14e47637005795b40c5efc5055b11be5d6d34fa
Orlando <1669785222@qq.com>
2024-11-10 16:49:14 +0800

    difference

--- Base/1-BasicAlgorithm/AC798.java
+++ Base/1-BasicAlgorithm/AC798.java
@@ -1,22 +1,32 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class AC798 {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] str = reader.readLine().split(" ");
 
         final int N = 1010;
 
-        int n = scanner.nextInt(), m = scanner.nextInt(), q = scanner.nextInt(), a[][] = new int[N][N], b[][] = new int[N][N];
+        int n = Integer.parseInt(str[0]), m = Integer.parseInt(str[1]), q = Integer.parseInt(str[2]), a[][] = new int[N][N], b[][] = new int[N][N];
 
-        for(int i = 1; i <= n; i++)
+        for(int i = 1; i <= n; i++){
+            str = reader.readLine().split(" ");
             for(int j = 1; j <= m; j++){
-                a[i][j] = scanner.nextInt();
+                a[i][j] = Integer.parseInt(str[j - 1]);
 
                 insert(b, i, j, i, j, a[i][j]);
             }
+        }
+            
         
         while(q > 0){
-            int x1 = scanner.nextInt(), y1 = scanner.nextInt(), x2 = scanner.nextInt(), y2 = scanner.nextInt(), c = scanner.nextInt();
+            str = reader.readLine().split(" ");
+            int x1 = Integer.parseInt(str[0]), y1 = Integer.parseInt(str[1]), x2 = Integer.parseInt(str[2]), y2 = Integer.parseInt(str[3]), c = Integer.parseInt(str[4]);
 
             insert(b, x1, y1, x2, y2, c);
 
@@ -25,13 +35,16 @@ public class AC798 {
                 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                 a[i][j] = a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1] + b[i][j];
-                 System.out.print(a[i][j] + " ");
+                a[i][j] = a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1] + b[i][j];
+                
+                writer.write(a[i][j] + " ");
             }
-            System.out.print('\n');
+            writer.write('\n');
         }
-        
-        scanner.close();
+
+        writer.flush();
+        reader.close();
+        writer.close();
     }
 
     private static void insert(int[][] b, int x1, int y1, int x2, int y2, int c){
