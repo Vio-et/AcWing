22e27a173e5a4d9122e9fd26196a6e5297d2bb80
Orlando <1669785222@qq.com>
2024-12-23 16:25:56 +0800

    2

---
+++ Base/DataStructure/AC826.java
@@ -0,0 +1,18 @@
+package Base.DataStructure;
+
+import java.util.Scanner;
+
+public class AC826{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        final int N = 100010;
+
+        int head = 2, idx = 2, e[] = new int[N], ne[] = new int[N];
+
+        System.out.println(head);
+        System.out.println(idx);
+
+        scanner.close();
+    }
+}
